fadd4a0b20d7e81518c2084ad17611c2f62d0417
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-15 16:31:52 +0200

    id removed not needed in this contract

--- ProductStore.Api/Contracts/UpdatProductsDto.cs
+++ ProductStore.Api/Contracts/UpdatProductsDto.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace ProductStore.Api.Contracts;
 
 public record class UpdateProductDto(
-    [Required] int Id,
     [Required][StringLength(50)] string Name,
     [Required][StringLength(50)] string Catagory,
     [Required] double Price,
