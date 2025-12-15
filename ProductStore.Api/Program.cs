c304ac381a3aa07de86ffeb7be47c76bfdfc9ee4
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-15 20:16:36 +0200

    setting up connection string for the Db Connection

--- ProductStore.Api/Program.cs
+++ ProductStore.Api/Program.cs
@@ -5,7 +5,7 @@ using ProductStore.Api.EndPoints;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var connStr = "Data Source=GameStore.db";
+var connStr = builder.Configuration.GetConnectionString("ProductStore");
 // start registering services to Db 
 // injecting services to the Db Context and Connect to the Databases with The Mapped Tables
 builder.Services.AddSqlite<ProductStoreContext>(connStr);
