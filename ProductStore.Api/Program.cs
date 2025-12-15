d5fafdc38e3201dc5b5f496fa9c33d6cc8ffdac2
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-15 16:36:48 +0200

    finished basic first version of Products CRUDs

--- ProductStore.Api/Program.cs
+++ ProductStore.Api/Program.cs
@@ -3,6 +3,7 @@ using ProductStore.Api.Contracts;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string productsRouteName = "GetProduct";
 List<ProductDto> products = [
     new(
         1,
@@ -31,7 +32,46 @@ List<ProductDto> products = [
 app.MapGet("products", () => products);
 
 // get game by id
-app.MapGet("products/{id}", (int id) => products.Find(p => p.Id == id));
+app.MapGet("products/{id}", (int id) => products.Find(p => p.Id == id)).WithName(productsRouteName);
+
+// creating[posting] a new product
+app.MapPost("products", (ProductDto newProduct) =>
+{
+    ProductDto product = new(
+        products.Count + 1,
+        newProduct.Name,
+        newProduct.Catagory,
+        newProduct.Price,
+        newProduct.ExpDate
+
+    );
+
+    products.Add(product);
+
+    return Results.CreatedAtRoute(productsRouteName, new { id = product.Id }, product);
+});
+
+// Update an existing product info by Id
+app.MapPut("products/{id}", (int id, UpdateProductDto updatedProduct) =>
+{
+    var index = products.FindIndex(p => p.Id == id);
+    products[index] = new ProductDto(
+       id,
+       updatedProduct.Name,
+       updatedProduct.Catagory,
+       updatedProduct.Price,
+       updatedProduct.ExpDate
+    );
+
+    return Results.NoContent();
+});
+
+//delete[remove] a product by Id
+app.MapDelete("products/{id}", (int id) =>
+{
+    products.RemoveAll(p => p.Id == id);
+    return Results.NoContent();
+});
 
 // roote route
 app.MapGet("/", () => "Welcome To Supplement Store");
