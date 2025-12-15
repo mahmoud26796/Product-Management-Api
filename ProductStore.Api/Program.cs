da6e190963dff345f237f6754e3b9d749c4651ba
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-15 21:26:46 +0200

    fixed very small pug was preventing migrations

--- ProductStore.Api/Program.cs
+++ ProductStore.Api/Program.cs
@@ -1,14 +1,15 @@
-using ProductStore.Api.Contracts;
 using ProductStore.Api.Data;
 using ProductStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 var connStr = builder.Configuration.GetConnectionString("ProductStore");
 // start registering services to Db 
 // injecting services to the Db Context and Connect to the Databases with The Mapped Tables
 builder.Services.AddSqlite<ProductStoreContext>(connStr);
+
+var app = builder.Build();
+
 app.MapProductsRoutes();
 
 app.Run();
