3233e5bde89f4ac2d46286c4dd91ab28f2e7b01d
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-20 16:11:13 +0200

    adding swagger to the Project

--- ProductStore.Api/Program.cs
+++ ProductStore.Api/Program.cs
@@ -7,8 +7,18 @@ var connStr = builder.Configuration.GetConnectionString("ProductStore");
 // start registering services to Db 
 // injecting services to the Db Context and Connect to the Databases with The Mapped Tables
 builder.Services.AddSqlite<ProductStoreContext>(connStr);
+// Swagger Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure Swagger in Dev Env
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Serves the Swagger JSON document
+    app.UseSwaggerUI(); // Serves the Swagger UI
+}
 
 // calling the Routes Mapping Method
 app.MapProductsRoutes();
