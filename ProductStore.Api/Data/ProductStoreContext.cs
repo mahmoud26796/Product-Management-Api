ab122d729f154bc6395e3542183784eee670a040
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-21 22:45:42 +0200

    fixed some typos

--- ProductStore.Api/Data/ProductStoreContext.cs
+++ ProductStore.Api/Data/ProductStoreContext.cs
@@ -11,13 +11,13 @@ public class ProductStoreContext(DbContextOptions<ProductStoreContext> options)
     public DbSet<Product> Products => Set<Product>();
 
     // Maps To Catagories Table in DB Context
-    public DbSet<Catagory> Catagories => Set<Catagory>();
+    public DbSet<Category> Catagories => Set<Category>();
 
     // seeding data so when application runs migrations those basic data will be always there to start with
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // creating each catagory name with the associated Id
-        modelBuilder.Entity<Catagory>().HasData(
+        modelBuilder.Entity<Category>().HasData(
             new { Id = 1, Name = "Protiens" },
             new { Id = 2, Name = "Performace" },
             new { Id = 3, Name = "Vitamins" }
