53043e8b49a176e0f1d583773ba73511ba56d385
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-12-22 21:18:13 +0200

    fixed typo in category name

--- ProductStore.Api/Contracts/ProductDto.cs
+++ ProductStore.Api/Contracts/ProductDto.cs
@@ -5,7 +5,7 @@ namespace ProductStore.Api.Contracts;
 public record class ProductDto(
     [Required] Guid Id,
     [Required][StringLength(50)] string Name,
-    [Required][StringLength(50)] string Catagory,
+    [Required][StringLength(50)] string Category,
     [Required] double Price,
     [Required] DateOnly ExpDate
 );
